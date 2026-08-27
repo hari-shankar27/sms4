5f637610ceb41cb72c9f6e9cd310b15714b5ca42
Hari Shankar Basnet <harishankarbasnet111@gmail.com>
2026-08-28 00:06:00 +0545

    footer

---
+++ footer.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="footer.css">
+     <link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+<body>
+    <footer class="footer">
+        <div class="footer-container">
+                <div class="top">
+                    <div class="brand">
+                        
+<a href="index.html" class="logofooter" >
+                 <div class="footerlogo">
+                    <i class="fa-solid fa-school"></i>
+                 </div>
+                 <div class="sname">
+                  <div class="ftitle">
+                       HariManage
+                             </div>
+                     <div class="footertitle">
+                       School Management System
+                     </div>
+                     </div>
+
+                </a>
+                    <p class="foo">  Empowering schools with modern technology,
+                    connected learning and smarter management.
+                    Building a better educational experience
+                    for students, teachers and administrators.</p>
+
+                    <div class="social">
+                        <a href="#" aria-label="Facebook">
+                            <i class="fa-brands fa-facebook-f"></i>
+                        </a>
+                        <a href="#" aria-label="Instagram">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                        <a href="#" aria-label="Twitter">
+                            <i class="fa-brands fa-x-twitter"></i>
+                        </a>
+
+                        <a href="#" aria-label="YouTube">
+                            <i class="fa-brands fa-youtube"></i>
+                        </a>
+                    </div>
+                </div>
+                    <div class="quick">
+                        <h3>Quicks Links</h3>
+
+                        <ul>
+                            <li>
+                                <a href="index.html"><i class="fa-solid fa-chevron-right"></i>
+                            Home</a>
+                            </li>
+
+                            <li>
+                                <a href="about.html"><i class="fa-solid fa-chevron-right"></i>About Us</a>
+                            </li>
+                         <li>
+                        <a href="#login">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            Login
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="register.html">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            Register
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="contact.html">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            Contact Us
+                        </a>
+                    </li>
+
+                        </ul>
+                    </div>
+                     <div class="quick">
+
+                <h3>Academics</h3>
+
+                <ul>
+
+                    <li>
+                        <a href="#">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            Classes
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            Subjects
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            Examination
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            Results
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fa-solid fa-chevron-right"></i>
+                            Attendance
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+             <div class="footer-column footer-contact">
+
+                <h3>Get In Touch</h3>
+
+
+                <div class="contact-item">
+
+                    <div class="contact-icon">
+
+                        <i class="fa-solid fa-location-dot"></i>
+
+                    </div>
+
+                    <div>
+
+                        <span>Address</span>
+
+                        <p>
+                           Biratnagar, Nepal
+                        </p>
+
+                    </div>
+
+                </div>
+
+
+                <div class="contact-item">
+
+                    <div class="contact-icon">
+
+                        <i class="fa-solid fa-phone"></i>
+
+                    </div>
+
+                    <div>
+
+                        <span>Phone</span>
+
+                        <a href="tel:+9779800000000">
+                            9812345678
+                        </a>
+
+                    </div>
+
+                </div>
+
+
+                <div class="contact-item">
+
+                    <div class="contact-icon">
+
+                        <i class="fa-solid fa-envelope"></i>
+
+                    </div>
+
+                    <div>
+
+                        <span>Email</span>
+
+                        <a href="mailto:school@gmail.com">
+                            school@gmail.com
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+        <div class="footer-newsletter">
+
+            <div class="newsletter-content">
+
+                <div class="newsletter-icon">
+
+                    <i class="fa-regular fa-envelope"></i>
+
+                </div>
+
+                <div>
+
+                    <h3>
+                        Stay Connected
+                    </h3>
+
+                    <p>
+                        Get school updates and important
+                        announcements directly in your inbox.
+                    </p>
+
+                </div>
+
+            </div>
+
+
+            <form
+                class="newsletter-form"
+                id="newsletterForm"
+            >
+
+                <input
+                    type="email"
+                    placeholder="Enter your email address"
+                    required
+                >
+
+                <button type="submit">
+
+                    Subscribe
+
+                    <i class="fa-solid fa-arrow-right"></i>
+
+                </button>
+
+            </form>
+
+        </div>
+
+        <div class="footer-bottom">
+
+
+            <p>
+
+                © <span id="currentYear"></span>
+
+                <strong>SchoolManage</strong>.
+
+                All rights reserved.
+
+            </p>
+
+
+            <div class="footer-bottom-links">
+
+                <a href="#">
+                    Privacy Policy
+                </a>
+
+                <span></span>
+
+                <a href="#">
+                    Terms & Conditions
+                </a>
+
+                
+
+            </div>
+
+                </div>
+        </div>
+        
+
+
+    </footer>
+
+</body>
+</html>
