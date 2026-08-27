3d350a53bdd6d12297f78faa359bdd2fff8df4f7
Hari Shankar Basnet <harishankarbasnet111@gmail.com>
2026-08-27 23:26:05 +0545

    hero

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+<body>
+<?php include 'navbar.php' ?>
+
+</div>
+<section class="hero">
+  <div class="hero-container">
+
+     <div class="left">
+        <div class="wel">
+            <span class="dot"></span>Welcome to HariManage
+        </div>
+<h1 class="title">Empowering Students.<br><span>Building Bright Futures.</span></h1>
+ <p class="dis">A modern school management platform designed to connect students, teachers, parents 
+    and administrators in one simple and powerful digital environment.</p>
+
+<div class="buttom">
+    <a href="program.html" class="explore">
+        Explore Programs 
+        <i class="fa-solid fa-arrow-right"></i>
+    </a>
+    <a href="about.html" class="learn">
+        <span>
+        <i class="fa-solid fa-play"></i></span>
+        Learn More
+    </a>
+</div>
+
+
+<div class="trust">
+    <div class="stpa">
+        <div class="s">
+            S
+        </div>
+        <div class="s">
+            T
+        </div>
+        <div class="s">
+            P
+        </div>
+        <div class="s">
+            A
+        </div>
+        </div>
+        <div class="trust-text">
+            <strong>Trusted School Community</strong>
+            <span>Connecting students and teachers</span>
+        </div>
+</div>
+
+</div>
+
+<div class="right">
+     <div class="image">
+        <img src="image/image.png" class="m">
+
+       <div class="success">
+        <div class="i">
+             <i class="fa-solid fa-trophy"></i>
+        </div>
+        <div>
+            <strong>98%</strong>
+            <span>Success Rate</span>
+        </div>
+       </div>
+        <div class="student">
+            <div class="ic">
+                 <i class="fa-solid fa-user-graduate"></i>
+            </div>
+            <div>
+                <strong>2500+</strong>
+                <span>Happy Students</span>
+            </div>
+
+        </div>
+
+     </div>
+</div>
+</div>
+
+  <div class="st">
+<div class="item">
+            <div class="icon">
+              <i class="fa-solid fa-user-graduate"></i>
+            </div>
+             <div>
+                <strong>2,500+</strong>
+                <span>Students</span>
+            </div>
+</div>
+
+
+<div class="item">
+            <div class="icon">
+              <i class="fa-solid fa-chalkboard-user"></i>
+            </div>
+             <div>
+                <strong>150+</strong>
+                <span>Teachers</span>
+            </div>
+
+
+</div>
+
+<div class="item">
+            <div class="icon">
+              <i class="fa-solid fa-award"></i>
+            </div>
+             <div>
+                <strong>25+</strong>
+                <span>Experience</span>
+            </div>
+
+
+</div>
+
+<div class="item">
+            <div class="icon">
+              <i class="fa-solid fa-building-columns"></i>
+            </div>
+             <div>
+                <strong>30+</strong>
+                <span>Programs</span>
+            </div>
+
+
+</div>
+
+  </div>
+    
+     
+
+</section>
+
+
+</body>
+</html>
